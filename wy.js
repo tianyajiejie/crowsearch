63db6f0e43faa667febda8e247ada77b38aecbaf
tianyajiejie <59963945+tianyajiejie@users.noreply.github.com>
2020-01-16 21:48:57 +0800

    Create wy.js

---
+++ wy.js
@@ -0,0 +1,9 @@
+// second DM , remove first DM
+search_url = "https://mijisou.com/?q=";
+
+// second DM
+search_box = '';
+search_box += '<div class="box"><a href="#"></a><p><img class="icon" src="home_book.png" alt=""></p><p class="url">在线小说</p></div>';
+search_box += '<div class="box"><a href="https://translate.google.cn"></a><p><img class="icon" src="home_translate.png" alt=""></p><p class="url">谷歌翻译</p></div>';
+search_box += '<div class="box"><a href="https://nanjiys.com/"></a><p><img class="icon" src="home_player.png" alt=""></p><p class="url">南极影院</p></div>';
+search_box += '<div class="box"><a href="#"></a><p><img class="icon" src="home_mark.png" alt=""></p><p class="url">推荐书签</p></div>';
