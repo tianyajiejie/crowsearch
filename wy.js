e22e332bad8b1db3408cca827ae5de63adca5497
tianyajiejie <59963945+tianyajiejie@users.noreply.github.com>
2020-01-17 13:58:52 +0800

    update wy
    update

--- wy.js
+++ wy.js
@@ -1,10 +1,10 @@
-// second DM
+// second engine DM
 search_engine = '';
 search_engine += '<label class="checkbox-inline"><input type="radio" name="ops_engine" id="engine4" value="https://m.mijisou.com/?q=" checked>秘迹</label>';
 search_engine += '<label class="checkbox-inline"><input type="radio" name="ops_engine" id="engine2" value="https://m.baidu.com/s?wd=">百度</label>';
 search_engine += '<label class="checkbox-inline"><input type="radio" name="ops_engine" id="engine1" value="https://cn.bing.com/search?q=">微软</label>';
 
-// second DM
+// second box DM
 search_box = '';
 search_box += '<div class="box"><a href="https://wuyayy.com/"></a><p><img class="icon" src="home_player.png" alt=""></p><p class="url">乌鸦影院</p></div>';
 search_box += '<div class="box"><a href="https://m.23wxc.com/"></a><p><img class="icon" src="home_book.png" alt=""></p><p class="url">顶点小说</p></div>';
